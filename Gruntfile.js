866d61c6d7203c6bb493f06f6fd83437e62f4e79
Allan Hortle <allan.hortle@blueflag.com.au>
2014-01-14 14:34:04 +1100

    added the ability to copy to locations via config.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,32 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      dist: {
-        src: [
-          '*.scss', 
-          '!tunic.scss'
-        ],
-        dest: 'tunic.scss'
-      }
-    }
-  });
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		config: grunt.file.readJSON('config.json'),
+		concat: {
+			dist: {
+				src: [
+					'*.scss', 
+					'!tunic.scss'
+				],
+				dest: 'tunic.scss'
+			}
+		},
+		copy: {
+			dist: {
+				files: [
+					{expand: true, src: ['tunic.scss'], dest: '<%= config.dir1 %>'},
+					{expand: true, src: ['tunic.scss'], dest: '<%= config.dir2 %>'}
+				]
+			}
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
-  // Tasks
-  grunt.registerTask('default', ['concat']);
+	// Tasks
+	grunt.registerTask('default', ['concat', 'copy']);
 
 };
