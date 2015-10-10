84aa5cc5b4cd03cc3d62df87f0f2a1dd4cd075f2
Allan Hortle <allanhortle@gmail.com>
2015-10-11 00:00:20 +1100

    more react templating

--- components/DocItem.jsx
+++ components/DocItem.jsx
@@ -3,15 +3,38 @@ var React = require('react');
 
 var DocItem = React.createClass({
     render: function () {
-        console.log(this.props);
-        var {context: {name, code}} = this.props;
+        // console.log(this.props);
+        var {
+            context: {name, code, type},
+            description,
+            group,
+            parameter
+        } = this.props;
         return (
             <div>
-                <h2>{name}</h2>
+                <h2>{name} <em>{type}</em></h2>
+                <h3>{group.join(',')}</h3>
+                <p>{description}</p>
+                <pre><code>{code}</code></pre>
 
-                <pre>{code}</pre>
+                {this.renderParameters()}
             </div>
         )
+    },
+    renderParameters() {
+        var {parameter} = this.props;
+        if (parameter) {
+            return <table>
+                <tbody>{parameter.map(this.renderParameterRows)}</tbody>
+            </table>;
+        }
+    },
+    renderParameterRows(row, key) {
+        return <tr key={key}>
+            <td>{row.name}</td>
+            <td>{row.type}</td>
+            <td>{row.description}</td>
+        </tr>
     }
 })
 
