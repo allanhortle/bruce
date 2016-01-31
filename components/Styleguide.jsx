9bb77076065d503d3fff46bfb4ec50fa153b8657
Allan Hortle <allanhortle@gmail.com>
2016-01-31 11:08:32 +1100

    large refactor, probs breaking changes

--- components/Styleguide.jsx
+++ components/Styleguide.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Prism from './Prism';
+import Markdown from './Markdown';
 
+function renderDescription(description) {
+	var arr = description.split('\n');
+	if(arr.length > 1) {
+		return <div>
+			<Markdown data={arr[0]}/>
+			<pre><Prism language="css">{arr.slice(1, arr.length-1).join('\n')}</Prism></pre>
+		</div>;
+	} else {
+		return <pre><Prism language="css">{arr[0]}</Prism></pre>
+	}
+
+}
 export default (props) => {
-	var {header, markup} = props;
-	return <div>
-		<h3>{header}</h3>
-		<pre><Prism language="css">{props.description}</Prism></pre>
+	var {header, markup, description} = props;
+	var descriptionArray = props.description.split('\n');
+	return <div id={header} className="DocItem">
+		<h2>{header}</h2>
+		{renderDescription(description)}	
 		<pre><Prism language="html">{markup}</Prism></pre>
 		<div className="Example" dangerouslySetInnerHTML={{__html: markup}}></div>
 	</div>;
